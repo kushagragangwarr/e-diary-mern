b418747d799a88f831c0e51dad952a262b9b7175
unknown <kushagragangwar4@gmail.com>
2021-04-23 12:15:13 +0530

    Added methods for adding updating and deleting notes

--- controllers/noteCtrl.js
+++ controllers/noteCtrl.js
@@ -29,21 +29,29 @@ const noteCtrl = {
     },
     deleteNote: async (req, res) => {
         try {
-
+            await Notes.findByIdAndDelete(req.params.id);
+            res.json({msg: 'Deleted a Note'});
         } catch(err) {
             return res.status(500).json({msg: err.message});
         }
     },
     updateNote: async (req, res) => {
         try {
-            
+            const {title, content, date} = req.body;
+            await Notes.findOneAndUpdate({_id: req.params.id}, {
+                title,
+                content,
+                date
+            });
+            res.json({msg: 'Note has been updated'});
         } catch(err) {
             return res.status(500).json({msg: err.message});
         }
     },
     getNote: async (req, res) => {
         try {
-            
+            const note = await Notes.findById(req.params.id);
+            res.json(note);
         } catch(err) {
             return res.status(500).json({msg: err.message});
         }
